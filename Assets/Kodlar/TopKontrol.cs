fdff563571a00094caf1eb658dce61b79d02fd42
Burak <brk.dskn@gmail.com>
2021-03-09 19:02:36 +0300

    finish project

---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    public int toplanilacakObjeSayisi;
+
+    public Text sayacText;
+    public Text oyunBittiText;
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay,0,dikey);
+
+        fizik.AddForce(vec*hiz);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="engel")
+        {
+            other.gameObject.SetActive(false);
+            sayac++;
+
+            sayacText.text = "Sayac = " + sayac;
+
+            if (sayac == toplanilacakObjeSayisi)
+            {
+                oyunBittiText.text = "Oyun Bitti";
+            }
+        }
+    }
+}
